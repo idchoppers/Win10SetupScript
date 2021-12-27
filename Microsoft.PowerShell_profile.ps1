0e40c70a57b47346568e043f6c6878a66efcfbcc
idchoppers <54718446+idchoppers@users.noreply.github.com>
2021-12-26 21:15:37 -0500

    Fix profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,6 @@ function Prompt
     Write-Host -ForegroundColor "Red" -NoNewLine ($env:USERNAME)
     Write-Host -ForegroundColor "DarkRed" -NoNewLine ":"
     Write-Host -ForegroundColor "Magenta" -NoNewLine (Get-Location)
-    Write-Host -ForegroundColor "White" -NoNewLine "> ")
+    Write-Host -ForegroundColor "White" -NoNewLine ">"
     return " "
 }
