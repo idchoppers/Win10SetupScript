4c1c0c6c1c4dfe6091b8e1e64c30ec2fd1bdaebd
idchoppers <54718446+idchoppers@users.noreply.github.com>
2021-12-26 21:26:52 -0500

    Update Colors

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,8 @@
 function Prompt
 {
-    Write-Host -ForegroundColor "DarkRed" -NoNewLine "["
+    Write-Host -ForegroundColor "Red" -NoNewLine "["
     Write-Host -ForegroundColor "Cyan" -NoNewLine (Get-Date -UFormat "%R")
-    Write-Host -ForegroundColor "DarkRed" -NoNewLine "] "
+    Write-Host -ForegroundColor "Red" -NoNewLine "] "
     Write-Host -ForegroundColor "Red" -NoNewLine ($env:USERNAME)
     Write-Host -ForegroundColor "DarkRed" -NoNewLine ":"
     Write-Host -ForegroundColor "Magenta" -NoNewLine (Get-Location)
