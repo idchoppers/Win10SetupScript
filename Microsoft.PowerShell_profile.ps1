d1a7a06a6b5055874d8f8af624af061915dd0340
idchoppers <54718446+idchoppers@users.noreply.github.com>
2021-12-27 08:13:36 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,9 @@ $Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
 Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
 function Prompt
 {
-    Write-Host -ForegroundColor "Red" -NoNewLine "["
+    Write-Host -ForegroundColor "DarkRed" -NoNewLine "["
     Write-Host -ForegroundColor "Cyan" -NoNewLine (Get-Date -UFormat "%R")
-    Write-Host -ForegroundColor "Red" -NoNewLine "] "
+    Write-Host -ForegroundColor "DarkRed" -NoNewLine "] "
     Write-Host -ForegroundColor "Red" -NoNewLine ($env:USERNAME)
     Write-Host -ForegroundColor "DarkRed" -NoNewLine ":"
     Write-Host -ForegroundColor "Magenta" -NoNewLine (Get-Location)
